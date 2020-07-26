5c871a26009b6326410023e244c7e30840c1e2c7
Jony <dev.jonykarki@gmail.com>
2020-07-26 23:19:45 +0000

    working efficientdet version

--- EfficientDet-D7/inference.py
+++ EfficientDet-D7/inference.py
@@ -6,43 +6,81 @@ sys.path.insert(0, os.path.join(CONFIG.CFG.BASEPATH, "Yet-Another-EfficientDet-P
 
 import torch
 import numpy as np 
+import cv2
+import matplotlib.pyplot as plt
 import pandas as pd
 from backbone import EfficientDetBackbone
 from efficientdet.utils import BBoxTransform, ClipBoxes
-from utils.utils import preprocess, postprocess
+from utils.utils import preprocess, postprocess, invert_affine
+from torch.backends import cudnn
 
-force_input_size = None
 compound_coef = 4
+force_input_size = None
+img_path = [os.path.join("../../data", "test", img_path) for img_path in os.listdir(os.path.join(CONFIG.CFG.DATA.BASE, "test"))]
+# IMG_PATH = os.path.join(CONFIG.CFG.DATA.BASE, "test", "2fd875eaa.jpg")
 
-anchor_ratios = [(1.0, 1.0), (1.4, 0.7), (0.7, 1.4)]
-anchor_scales = [2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)]
+threshold = 0.2
+iou_threshold = 0.2
 
-model = EfficientDetBackbone(compound_coef=4, num_classes=1, ratios=anchor_ratios, scales=anchor_scales)
-model.load_state_dict(torch.load("efficientdet-d4_14_20235.pth"))
+use_cuda = True
+use_float16 = False
+cudnn.fastest = True
+cudnn.benchmark = True
+
+obj_list = ['wheat']
+
+# tf bilinear interpolation is different from any other's, just make do
+input_sizes = [512, 640, 768, 896, 1024, 1280, 1280, 1536]
+input_size = input_sizes[compound_coef] if force_input_size is None else force_input_size
+ori_imgs, framed_imgs, framed_metas = preprocess(*img_path, max_size=input_size)
+
+if use_cuda:
+    x = torch.stack([torch.from_numpy(fi).cuda() for fi in framed_imgs], 0)
+else:
+    x = torch.stack([torch.from_numpy(fi) for fi in framed_imgs], 0)
+
+x = x.to(torch.float32 if not use_float16 else torch.float16).permute(0, 3, 1, 2)
+
+model = EfficientDetBackbone(compound_coef=compound_coef, num_classes=len(obj_list),
+
+                             # replace this part with your project's anchor config
+                             ratios=[(1.0, 1.0), (1.4, 0.7), (0.7, 1.4)],
+                             scales=[2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)])
+
+model.load_state_dict(torch.load('efficientdet-d4_14_20235.pth'))
 model.requires_grad_(False)
 model.eval()
 
-IMG_PATH = [os.path.join(CONFIG.CFG.DATA.BASE, "test", img_path) for img_path in os.listdir(os.path.join(CONFIG.CFG.DATA.BASE, "test"))]
-
-input_sizes = [512, 640, 768, 896, 1024, 1280, 1280, 1536]
-input_size = input_sizes[compound_coef] if force_input_size is None else force_input_size
-
-ori_imgs, framed_imgs, framed_metas = preprocess(*IMG_PATH, max_size=input_size)
-
-DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-model.to(DEVICE)
-
-x = torch.stack([torch.from_numpy(fi).to(DEVICE) for fi in framed_imgs], 0)
-
-x.to(torch.float32).permute(0, 3, 1, 2)
+if use_cuda:
+    model = model.cuda()
+if use_float16:
+    model = model.half()
 
 with torch.no_grad():
     features, regression, classification, anchors = model(x)
 
     regressBoxes = BBoxTransform()
-    ClipBoxes = ClipBoxes()
+    clipBoxes = ClipBoxes()
 
-    out = postprocess(x, anchors, regression, classification, regressBoxes, ClipBoxes, 0.2, 0.2)
+    out = postprocess(x,
+                      anchors, regression, classification,
+                      regressBoxes, clipBoxes,
+                      threshold, iou_threshold)
 
-    print(out)
+out = invert_affine(framed_metas, out)
+
+for i in range(len(ori_imgs)):
+    if len(out[i]['rois']) == 0:
+        continue
+    
+    for j in range(len(out[i]['rois'])):
+        (x1, y1, x2, y2) = out[i]['rois'][j].astype(np.int)
+        cv2.rectangle(ori_imgs[i], (x1, y1), (x2, y2), (255, 255, 0), 2)
+        obj = obj_list[out[i]['class_ids'][j]]
+        score = float(out[i]['scores'][j])
+
+        cv2.putText(ori_imgs[i], '{}, {:.3f}'.format(obj, score),
+                    (x1, y1 + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    (255, 255, 0), 1)
+
+        plt.imshow(ori_imgs[i])
